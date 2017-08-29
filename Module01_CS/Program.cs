01c895c6d254bc0fc312e4d03b9969c37284a6cf
admin <admin@AR-113-01>
2017-08-29 14:32:09 +0200

    gggggg.

---
+++ Module01_CS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Module01_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Dette er en test");
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
