7e9092a25ce5c1cad9cc9c07efb7b1be3f572a5a
hvagit <henrik1969@gmail.com>
2017-09-12 15:57:16 +0200

    Abc

---
+++ Trekant/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trekant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trekant t = new Trekant(7, 4);
+            Console.WriteLine(t.Areal);
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("PRESS ANY KEY TO CONTINUE");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    class Trekant
+    {
+        public int Grundlinje { get; private set; }
+        public int Højde { get; private set; }
+        public Trekant(int h, int g)
+        {
+            Grundlinje = g;
+            Højde = h;
+        }
+
+        // Denne form vælges, da der skal gøres noget i getter-metoden for denne egenskab.
+        public double Areal {
+            get
+            {
+                    return 0.5 * this.Grundlinje * this.Højde;
+            }
+        }
+    }
+}
---
+++ Vare/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vare v = new Vare(12.50, "Corn Flakes");
+            Console.WriteLine(v.PrisMedMoms());
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("PRESS ANY KEY TO CONTINUE");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    class Vare
+    {
+        private string navn;
+
+        public string Navn
+        {
+            get
+            {
+                Console.WriteLine("Getter navn");
+                return navn;
+            }
+            set
+            {
+                Console.WriteLine("Setter navn");
+                navn = value;
+            }
+        }
+
+        private double pris;
+
+        public double Pris
+        {
+            get
+            {
+                Console.WriteLine("Getter pris");
+                return pris;
+            }
+            set
+            {
+                Console.WriteLine("Setter pris");
+                pris = value;
+            }
+        }
+
+        public Vare()
+        {
+            this.Pris = 0.0;
+            this.Navn = "";
+        }
+
+        public Vare(double pris, string navn)
+        {
+            this.Pris = pris;
+            this.Navn = navn;
+        }
+
+        public double PrisMedMoms()
+        {
+            return this.Pris * 1.25;
+        }
+
+
+    }
+
+}
